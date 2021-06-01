1520d0ce391b636a3fed52d061f893c59d70c16f
Carlos <sirfuentes7@gmail.com>
2021-06-01 07:38:37 +0200

    english and spanish language

--- scrumapp-jsf/src/main/java/org/cfuentes/scrumapp/controller/GlobalController.java
+++ scrumapp-jsf/src/main/java/org/cfuentes/scrumapp/controller/GlobalController.java
@@ -1,5 +1,13 @@
 package org.cfuentes.scrumapp.controller;
 
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
 import org.cfuentes.scrumapp.entity.Proyecto;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
@@ -10,6 +18,28 @@ public class GlobalController {
 
 	private Object entidad;
 	private Proyecto proyecto;
+	private LinkedHashMap<String, Object> langs;
+	private Locale lang;
+	
+	public void setSpanish() {
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale ( "es" , "ES" ));
+	}
+	
+	public void setEnglish() {
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(Locale.ENGLISH);
+	}
+	
+	public void localeChanged(ValueChangeEvent e) {
+	      String newLocaleValue = e.getNewValue().toString();
+	      
+	      for (Map.Entry<String, Object> entry : langs.entrySet()) {
+	         
+	         if(entry.getValue().toString().equals(newLocaleValue)) {
+	            FacesContext.getCurrentInstance()
+	               .getViewRoot().setLocale((Locale)entry.getValue());         
+	         }
+	      }
+	   }
 	
 	public Proyecto getProyecto() {
 		return proyecto;
@@ -25,6 +55,22 @@ public class GlobalController {
 
 	public void setEntidad(Object entidad) {
 		this.entidad = entidad;
+	}
+
+	public LinkedHashMap<String, Object> getLangs() {
+		return langs;
+	}
+
+	public void setLangs(LinkedHashMap<String, Object> langs) {
+		this.langs = langs;
+	}
+
+	public Locale getLang() {
+		return lang;
+	}
+
+	public void setLang(Locale lang) {
+		this.lang = lang;
 	}
 	
 	
